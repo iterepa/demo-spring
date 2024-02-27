c1c34fe197809f06deaf8eeafde4c777ac52bc03
frogiri <wizmond705@gmail.com>
2024-02-27 13:05:01 +0200

    initial gitHub commit

---
+++ 01-spring-boot-overview/01-spring-boot-demo/src/test/java/com/studying/springboot/demo/myTestApp/MyTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studying.springboot.demo.myTestApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/com/studying/springboot/demo/myTestApp/MyTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.studying.springboot.demo.myTestApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTestAppApplication.class, args);
+	}
+
+}
